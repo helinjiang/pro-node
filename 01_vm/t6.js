233f9e4a2f2e37f87284e96cc499671e046a5ed8
linjianghe <linjianghe@tencent.com>
2018-05-27 15:46:17 +0800

    feat: 如果使用了 import ，就需要用到 babel 处理之后再调用

---
+++ 01_vm/t6.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const babel = require('babel-core');
+
+const { NodeVM } = require('vm2');
+
+// 如果引入了外部模块，则不能够设置 require.root 属性，否则将会提示没有权限引入
+// VMError: Module 'mockjs' is not allowed to be required. The path is outside the border!
+// 可以通过控制 external 来限制哪些外部包可以被引用
+const vm = new NodeVM({
+    sandbox: {},
+    require: {
+        external: true,
+        builtin: ['fs', 'path']
+        // root: __dirname
+    }
+});
+
+let TARGET_PATH = path.resolve('./data/user_info/handle_modules/success_import/index.js');
+
+fs.readFile(TARGET_PATH, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+
+    let result = babel.transform(data, {
+        presets: ['env', 'stage-0']
+    });
+
+    console.log(result.code);
+
+    let functionInSandbox = vm.run(result.code, TARGET_PATH);
+
+    console.log('====result====', functionInSandbox.default());
+});
